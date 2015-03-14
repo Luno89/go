65fbafbe725beef553a67c3f112e3abde892f439
Corigan Simpson <corigan.simpson@gmail.com>
2015-03-14 07:38:00 -0400

    added carousel data structs

--- kcpoolsite/pool.go
+++ kcpoolsite/pool.go
@@ -11,7 +11,41 @@ import (
     "errors"
 )
 
-var templates = template.Must(template.ParseFiles("tmpl/home.html"))
+// carousel data
+type carousel struct {
+	Name string
+	Indicators []indicator
+	Items []item
+}
+
+// slide item data
+type item struct {
+	Src string
+	Alt string
+	Active string
+	Caption byte[]
+}
+
+// indicator item data
+type indicator struct {
+	Name string
+	Index int
+	Active string
+}
+
+
+var templates = template.Must(template.ParseFiles("tmpl/home.html","tmpl/carousel.tmpl"))
 var validPath = regexp.MustCompile("^/(home|view)/([a-zA-z0-9]+)$")
 
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, , http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home", )
+}
